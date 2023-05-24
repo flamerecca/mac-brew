5e1121a6ef64a32c4eab5c4652666aec52073ee3
recca <flamerecca711@gmail.com>
2023-05-24 15:55:51 +0800

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,6 +1,19 @@
 !/bin/bash
 /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
 brew install php
-brew cask install java
-brew cask install squirrel
-brew cask install phpstorm
+brew install nmap
+brew install kerl
+brew install telnet
+brew install ftp
+brew install gradle
+brew install node
+brew install exa
+brew install bat
+brew install tldr
+brew install ag
+brew install jq
+brew install --cask minikube
+brew install --cask java
+brew install --cask squirrel
+brew install --cask phpstorm
+brew install --cask dbeaver-community
