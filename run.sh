5e8df030639e9d911b055f08b7994b402d2ac912
recca <flamerecca711@gmail.com>
2023-07-11 17:20:09 +0800

    Update run.sh

--- run.sh
+++ run.sh
@@ -20,7 +20,6 @@ brew install terraform
 brew install terragrunt
 brew install cmctl
 brew install pnpm
-brew install zsh
 brew install --cask java
 brew install --cask squirrel
 brew install --cask jetbrains-toolbox
