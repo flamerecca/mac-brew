bd2b8a120c1dc90018079f862633440e9185f6f3
recca <flamerecca711@gmail.com>
2024-09-02 10:30:33 +0800

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,38 +1,48 @@
 !/bin/bash
 /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
-brew install zsh
-brew install wget
-brew install php
-brew install nmap
-brew install kerl
-brew install telnet
-brew install lftp
+
+brew install alda
+brew install bat
+brew install bottom
+brew install cask
+brew install cmatrix
+brew install cmctl
+brew install docker
+brew install docker-compose
+brew install gnupg
 brew install gradle
-brew install mongodb
+brew install guile
+brew install htmlq
+brew install htop
+brew install httpie
+brew install jq
+brew install k6
+brew install kerl
+brew install ki
+brew install kubectx
+brew install lftp
+brew install minikube
+brew install nasm
+brew install nghttp2
+brew install nmap
 brew install node
 brew install nvm
-brew install bat
-brew install tldr
-brew install ag
-brew install jq
-brew install maccy
-brew install tree
-brew install kubectl
-brew install kubectx
-brew install minikube
-brew install terraform
-brew install terragrunt
-brew install cmctl
+brew install perl
+brew install php
 brew install pnpm
-brew install bottom
+brew install python@3.9
 brew install ripgrep
-brew install cmatrix
-brew install sqlmap
-brew install htop
-brew install btop
-brew install httpie
-brew install k6
-brew install gpg
+brew install ruby
+brew install swaks
+brew install telnet
+brew install terragrunt
+brew install the_silver_searcher
+brew install tig
+brew install tldr
+brew install tree
+brew install wget
+brew install zsh
+
 brew install --cask java
 brew install --cask squirrel
 brew install --cask jetbrains-toolbox
