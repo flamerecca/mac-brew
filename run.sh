a32955102211ab3fa019a4966181accc87517719
recca <flamerecca711@gmail.com>
2025-07-17 12:32:14 +0800

    Update run.sh

--- run.sh
+++ run.sh
@@ -76,3 +76,6 @@ curl -s "https://get.sdkman.io" | bash
 
 git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ~/powerlevel10k
 echo 'source ~/powerlevel10k/powerlevel10k.zsh-theme' >>~/.zshrc
+
+npm install -g @anthropic-ai/claude-code
+npm install -g @google/gemini-cli
